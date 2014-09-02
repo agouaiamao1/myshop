5f608751d10ef22a3e57ab12496f38c45db5c30b
shiyan_xiaobin <shiyan_xiaobin@163.com>
2014-09-03 07:38:34 +0800

    data

---
+++ data/cache/config.inc.php
@@ -0,0 +1,15 @@
+<?php if (!defined('HDPHP_PATH')) exit; 
+return array (
+  'webname' => '内容管理系统',
+  'icp' => '京ICP备12048441号-3',
+  'copyright' => 'Copyright © 2012-2013 .........版权所有',
+  'keywords' => '永恒  ',
+  'description' => '永恒之塔yon',
+  'email' => 'shiyan_xiaobin@foxmail.com',
+  'web_open' => '1',
+  'tel' => '010-64825057',
+  'web_style' => 'hdjs',
+  'qq' => '345533112',
+  'weibo' => 'houdunwangxj@gmail.com',
+);
+?>
---
+++ data/cache/goods_type.php
@@ -0,0 +1,14 @@
+<?php if(!defined('HDPHP_PATH'))exit;
+return array (
+  0 => 
+  array (
+    'gid' => '1',
+    'gtname' => '鞋子',
+  ),
+  1 => 
+  array (
+    'gid' => '2',
+    'gtname' => '电器',
+  ),
+);
+?>
