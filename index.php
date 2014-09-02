4e8901794e944018d33d66c306ffe8784fcca6b3
shiyan_xiaobin <shiyan_xiaobin@163.com>
2014-09-03 07:36:01 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-type:text/html;charset=utf-8');
+define('GROUP_NAME','letao');//应用组名称
+define('GROUP_PATH','./myletao/');
+define('CACHE_PATH','data/cache/');//缓存目录
+define('DEBUG',true);
+require './hdphp/hdphp/hdphp.php';
+?>
